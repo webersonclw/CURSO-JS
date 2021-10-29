9ec9360e57a1033c84ca60f2ca2bb6dc76d27838
Weberson Santos (TELÓ) <webersonclw@yahoo.com.br>
2021-10-28 23:09:03 -0300

    p

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade de seu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log('Voce Ultapassou a velocidade limite')
+}
+console.log('Dirija sempre com sinto de segurança')
---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 16
+console.log(`Voce tem${iade} anos`)
+if (idade < 16) {
+    console.log('Não Vota')
+} else  if (idade <18 || idade > 65) {
+        console.log('Voto Opcional')
+} else {
+    console.log('Voto Obrigatorio')
+}
