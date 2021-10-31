5b624a6a11df9a65895f9c8ece46d5df8bdf8641
Weberson Santos (TELÓ) <webersonclw@yahoo.com.br>
2021-10-31 18:33:32 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,7 @@
-let num = []
-
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
 console.log(num)
+console.log(`O vetor tem ${num.length} Posições.`)
+console.log(`o Primeiro valor do vetor é ${num[0]}`)
+
