93ff3ec2b8f5edcef7593dab0e931fffd1cb20b8
Weberson Santos (TELÓ) <webersonclw@yahoo.com.br>
2021-10-31 19:07:10 -0300

    v

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -5,3 +5,9 @@ console.log(num)
 console.log(`O vetor tem ${num.length} Posições.`)
 console.log(`o Primeiro valor do vetor é ${num[0]}`)
 
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O Valor não foi encontrado!')
+}else {
+    console.log(`O valor 4 esta na posição ${pos}`)
+}
